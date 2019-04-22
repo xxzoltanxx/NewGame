f1bb85b17148bdb96240c738a72e0d688a811267
xxzoltanxx <antonio.franjcic1@gmail.com>
2019-04-22 03:56:18 +0200

    Affinity bar tweaks

--- Assets/Scripts/AffinityBar.cs
+++ Assets/Scripts/AffinityBar.cs
@@ -35,7 +35,7 @@ public class AffinityBar : MonoBehaviour
         //transform.GetChild(1).gameObject.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, scale);
         transform.GetChild(0).localScale = new Vector3(scale, scale, 1.0f);
         //transform.GetChild(1).GetChild(0).gameObject.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, scale);
-        transform.GetChild(0).GetChild(0).gameObject.GetComponent<MeshRenderer>().material.SetFloat("_Globalopacity", opacity);
+        transform.GetChild(0).GetChild(0).gameObject.GetComponent<MeshRenderer>().material.SetFloat("_Globalopacity", 1.0f);
         Color color = transform.GetChild(0).gameObject.GetComponent<Image>().color;
         transform.GetChild(0).gameObject.GetComponent<Image>().color = new Color(color.r, color.g, color.b, opacity);
     }
