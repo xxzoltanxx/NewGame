11c2362d3375ef4662d5c54b7ec718dd5344c638
xxzoltanxx <antonio.franjcic1@gmail.com>
2019-04-22 18:11:47 +0200

    Almost no progress today, paramhrough menueters and clickt

---
+++ Assets/Scripts/UIDissapearMenuClickHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class UIDissapearMenuClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    public RMF_RadialMenu menu;
+    private void Awake()
+    {
+        menu = GameObject.Find("radialMenuSkills").GetComponent<RMF_RadialMenu>();
+    }
+
+    public void OnPointerClick(PointerEventData data)
+    {
+        if (data.button == PointerEventData.InputButton.Right)
+        {
+            menu.handler.menuShown = false;
+            menu.close();
+        }
+    }
+}
